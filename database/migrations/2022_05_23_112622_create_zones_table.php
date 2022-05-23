a2cdac9a97f0ee92fcd9b5012b668c69186d1ecc
Shatha-Asidah <shathaasidah@gmail.com>
2022-05-23 14:49:34 +0300

    jops _ trainings _ zones
    create tables

--- database/migrations/2022_05_23_112622_create_zones_table.php
+++ database/migrations/2022_05_23_112622_create_zones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateZonesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('zones', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('location');
+            $table->integer('phone');
+            $table->text('available_times');
+            $table->text('description');
+            $table->bigInteger('charity_id');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('zones');
     }
 }
