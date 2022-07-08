f7a368d3015837554703fd6c16a9d77333c3e7c7
Abeer Asidah <abeerasidah@gmail.com>
2022-07-08 15:42:22 +0300

    aa

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -37,11 +37,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('admin/login',[\App\Http\Controllers\PassportAuthController::class,'adminLogin'])->name('adminLogin');
 
+
 Route::group( ['prefix' => 'admin','middleware' => ['auth:admin-api','scopes:admin'] ],function(){
    // authenticated staff routes here 
     Route::get('dashboard',[PassportAuthController::class,'adminDashboard']);
     Route::get('logout',[PassportAuthController::class,'adminlogout'])->name('adminLogout');
-
+    Route::post('delete/{id}', [\App\Http\Controllers\PassportAuthController::class, 'destroy']);
     
     
 
